3fd894be073fba9a61c5413d3258bdd338e7e377
Mit <mit4sheth@gmail.com>
2026-02-09 22:18:31 +0530

    Create ReactotronConfig.ts

---
+++ frontend/ReactotronConfig.ts
@@ -0,0 +1,53 @@
+import Reactotron, { ReactotronReactNative } from 'reactotron-react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const noop = () => undefined;
+
+const reactotron : ReactotronReactNative = Reactotron.setAsyncStorageHandler(AsyncStorage)
+  .configure({
+    name: 'VisionAI',
+    host: 'localhost',
+  })
+  .useReactNative({
+    asyncStorage: true,
+    networking: {
+      ignoreUrls: /symbolicate/,
+    },
+    editor: false,
+    errors: { veto: () => false },
+    overlay: false,
+  })
+  .connect();
+
+if (__DEV__) {
+  console.tron = reactotron;
+} else {
+  console.tron = {
+    configure: noop,
+    connect: noop,
+    use: noop,
+    useReactNative: noop,
+    clear: noop,
+    log: noop,
+    logImportant: noop,
+    display: noop,
+    error: noop,
+  } as unknown as typeof reactotron;
+}
+
+reactotron.onCustomCommand({
+  title: 'Show Dev Menu',
+  description: 'In Expo: shake device or press Cmd+D (iOS) / Cmd+M (Android)',
+  command: 'showDevMenu',
+  handler: () => {
+    Reactotron.log('Shake device or press Cmd+D (iOS) / Cmd+M (Android) to open Expo dev menu');
+  },
+});
+
+declare global {
+  interface Console {
+    tron: typeof reactotron;
+  }
+}
+
+export default reactotron;
