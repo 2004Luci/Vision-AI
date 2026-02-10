fe2431d37e8c57fba840a8d79881ee66bf6c4a82
Mit <mit4sheth@gmail.com>
2026-02-10 18:20:09 +0530

    add lottie

---
+++ frontend/src/animations/components/Lottie/Lottie.types.ts
@@ -0,0 +1,13 @@
+import type { AnimationObject } from 'lottie-react-native';
+import type { ViewStyle } from 'react-native';
+
+export interface LottieProps {
+  source: AnimationObject;
+  autoPlay?: boolean;
+  loop?: boolean;
+  resizeMode?: 'cover' | 'contain' | 'center';
+  onAnimationFinish?: () => void;
+  style?: ViewStyle;
+  width?: number;
+  height?: number;
+}
