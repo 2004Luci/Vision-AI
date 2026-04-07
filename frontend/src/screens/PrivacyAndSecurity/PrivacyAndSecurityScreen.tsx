1de710fdbfbb88e60019cddbf48664845120e362
Mit <mit4sheth@gmail.com>
2026-04-07 15:17:52 +0530

    fix nav flow for nested screens in settings stack

---
+++ frontend/src/screens/PrivacyAndSecurity/PrivacyAndSecurityScreen.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const PrivacyAndSecurityScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-white">PrivacyAndSecurityScreen</Text>
+    </View>
+  );
+};
+
+export default PrivacyAndSecurityScreen;
